866b5527868a445684c94cbbd6adfa469e8032e4
Kor de Jong <kor@jemig.eu>
2017-05-12 12:56:38 +0200

    Add reproject_raster

--- source/test/data_tools_test.py
+++ source/test/data_tools_test.py
@@ -1,14 +1,18 @@
 import os
 import unittest
+import numpy
 from numpy.testing import assert_array_equal
 import png
 import rasterio
+import rasterio.warp
 import tempfile
-from nc_data_tools.data_tools import \
-    convert_graphics_file_to_geotiff, \
-    is_name_of_geotiff_file, \
-    is_name_of_graphics_file, \
-    geotiff_pathname
+from nc_data_tools.data_tools import *
+
+#     convert_graphics_file_to_geotiff,
+#     is_name_of_geotiff_file,
+#     is_name_of_graphics_file,
+#     geotiff_pathname,
+#     reproject_raster
 
 
 class DataToolsTest(unittest.TestCase):
@@ -92,6 +96,62 @@ class DataToolsTest(unittest.TestCase):
             assert_array_equal(a, [[255,255,255], [255,255,255]])
 
         os.remove(raster_pathname)
+
+
+    def test_reproject_raster(self):
+        # Given a geotiff in EPSG:3857, reproject it in EPSG:28992
+
+        # Create geotiff in EPSG:3857
+        source_pathname = "raster-3857.tif"
+        source_crs = "EPSG:3857"
+        nr_rows = 3
+        nr_cols = 2
+        cell_size = 10.0
+        west = 0.0
+        north = 0.0 + nr_rows * cell_size
+        gdal_transformation = (west, cell_size, 0.0, north, 0.0, -cell_size)
+        transformation = rasterio.Affine.from_gdal(*gdal_transformation)
+
+        dtype = numpy.int32
+        profile = {
+            "driver": "GTiff",
+            "width": nr_cols,
+            "height": nr_rows,
+            "dtype": dtype,
+            "count": 1,
+            "crs": "EPSG:3857",
+            "transform": transformation,
+            "nodata": -999
+        }
+
+        with rasterio.open(source_pathname, "w", **profile) as source_raster:
+            source_raster.write(
+                numpy.array([[-1, 1], [0, -999], [1, 2]], dtype=dtype), 1)
+
+
+        # Verify projection
+        with rasterio.open(source_pathname) as source_raster:
+            self.assertEqual(source_raster.crs,
+                rasterio.crs.CRS.from_string(source_crs))
+
+
+        # Reproject
+        target_pathname = "raster-28992.tif"
+        target_crs = "EPSG:28992"
+
+        reproject_raster(source_pathname, target_pathname, target_crs,
+            rasterio.warp.RESAMPLING.nearest)
+
+
+        # Verify new projection
+        with rasterio.open(target_pathname) as target_raster:
+            self.assertEqual(target_raster.crs,
+                rasterio.crs.CRS.from_string(target_crs))
+            self.assertEqual(target_raster.width, nr_cols)
+            self.assertEqual(target_raster.height, nr_rows)
+
+        os.remove(source_pathname)
+        os.remove(target_pathname)
 
 
 if __name__ == "__main__":
